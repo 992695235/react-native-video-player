105a89b571b4e1c20eb5f72fd8de576c6751a09e
晴天 <992695235@qq.com>
2018-11-19 12:34:28 +0800

    replace vod lib a2.2.4.0

--- example/App.js
+++ example/App.js
@@ -19,7 +19,7 @@ class HomeScreen extends Component {
 //rtmp://live.hkstv.hk.lxdns.com/live/hks  http://192.168.77.35:8000/iphone7.mp4 /storage/emulated/0/Download/iphone7.mp4 /storage/emulated/0/Download/bbb_720p_qy265.flv
   constructor(props) {
       super(props);
-      this.state = { text: 'http://videohy.tc.qq.com/vlive.qqvideo.tc.qq.com/AyTHfm7kg4_gsclMzcgYGqJvWqxVdlIvEAbZv7g4aOVA/uwMRJfz-r5jAYaQXGdGnC2_ppdhgmrDlPaRvaV7F2Ic/a0786ort1o3.p701.1.mp4?sdtfrom=v1105&guid=36b65aceb9fef479c2725a629e38ef68&vkey=71EB94690908DF995CD8AEE2832BDB0E51004B00F66F97BF31504E68EE7AE93F3C036AA13C1E7B434D375629EA12D088B52D17728871045BFAA984D9CD14CA1F91F1BA56C1FE779C33D3EF9F64D7A6E91FD2403D5B895AB107A93DF900ACC513072D3C6060E2DA7FF11DD9F327EB4195D30CE124D03E69C6&ocid=892539820' };
+      this.state = { text: 'rtmp://10.20.5.145:1936/vod/file/local://D:/fms_TRMS/data/file/20180827/multi/11/b445aee85d2841518bd6ec0f88db5774.mp4' };
     }
 
   render() {
